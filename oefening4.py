817607601b613833addaf41845658dff5710d141
Nell Du Bois <nell.dubois@leerling.athenea.be>
2025-03-28 13:44:41 +0100

    hey mag ik een 10/10

--- oefening4.py
+++ oefening4.py
@@ -1,1 +1,17 @@
-def countTargetPairs():
+def countTargetPairs(nums, target):
+    aantal = 0
+    lengte = len(nums)
+   
+    for i in range(lengte):
+        for j in range(i + 1, lengte):
+            if nums[i] + nums[j] < target:
+                aantal += 1
+   
+    return aantal
+
+
+voorbeeld1 = countTargetPairs([-1, 1, 2, 3, 1], 2)  
+voorbeeld2 = countTargetPairs([-6, 2, 5, -2, -7, -1, 3], -2)  
+
+print(voorbeeld1)  
+print(voorbeeld2)
